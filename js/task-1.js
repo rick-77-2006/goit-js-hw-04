5bdb8d869d2cbea68535f93e518d288fa2971f4b
Rick <46992687+rick-77-2006@users.noreply.github.com>
2023-12-01 11:04:49 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -6,31 +6,31 @@
 
 
 
-// function isEnoughCapacity(products, containerSize) {
-//   let message;
+function isEnoughCapacity(products, containerSize) {
+  let message;
   
-// if (products > containerSize)
-//   {
-//   message = "Не достаточно местав в контейнере!";
-// }else{
-//   message = "Места в контейнере достаточно.";
-// }
-//   return message;
-// }
+if (products > containerSize)
+  {
+  message = "Не достаточно местав в контейнере!";
+}else{
+  message = "Места в контейнере достаточно.";
+}
+  return message;
+}
 
-// console.log(
-//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-// ); // true
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
-// console.log(
-//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-// ); // false
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
-// console.log(
-//   isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-// ); // true
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
 
-// console.log(
-//   isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-// ); // false
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
 
