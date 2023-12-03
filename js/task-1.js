d02ecf78016156a6d0e54381311bf2458145eb46
Rick <46992687+rick-77-2006@users.noreply.github.com>
2023-12-03 09:06:35 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -15,11 +15,6 @@ console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
 console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
 
-
-
-
-
-
 // const globalValue = 10;
 // function foo() {
 //   const a = 20;
