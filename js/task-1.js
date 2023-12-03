30a0b048f3b12d21670d12ee6a58d4cbafd9cc33
Rick <46992687+rick-77-2006@users.noreply.github.com>
2023-12-03 08:54:56 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,45 +1,34 @@
-const globalValue = 10;
-
-function foo() {
-  const a = 20;
-  console.log(a);
-  console.log(globalValue);
-
-  for (let i = 0; i < 5; i+= 1) {
-    console.log(a);
-    console.log(globalValue);
-
-    if (i === 2) {
-      console.log(a);
-      console.log(globalValue);
+function isEnoughCapacity(products, containerSize) {
+  const quantities = Object.values(products);
+  let totalQuantity = 0;
+  for (let quantity of quantities) {
+    totalQuantity += quantity;
+    if (totalQuantity > containerSize) {
+      return false;
     }
   }
+  return true;
 }
 
-// function isEnoughCapacity(products, containerSize) {
-//   let message;
-  
-// if (products > containerSize)
-//   {
-//   message = "Не достаточно местав в контейнере!";
-// }else{
-//   message = "Места в контейнере достаточно.";
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
+
+// const globalValue = 10;
+
+// function foo() {
+//   const a = 20;
+//   console.log(a);
+//   console.log(globalValue);
+
+//   for (let i = 0; i < 5; i+= 1) {
+//     console.log(a);
+//     console.log(globalValue);
+
+//     if (i === 2) {
+//       console.log(a);
+//       console.log(globalValue);
+//     }
+//   }
 // }
-//   return message;
-// }
-
-// console.log(
-//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-// ); // false
-
-// console.log(
-//   isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-// ); // false
